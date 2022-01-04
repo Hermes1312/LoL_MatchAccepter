6bcdf1804f34c6a39d5f00690777e37b5ac7065e
Her Mes <hermi1312@protonmail.com>
2022-01-04 17:28:43 +0100

    Add project files.

---
+++ LoL_MatchAccepter/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿
+namespace LoL_MatchAccepter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.rsizeTB = new System.Windows.Forms.TextBox();
+            this.rposTB = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(9, 332);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(274, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Save as";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(269, 114);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 146);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "PREVIEW";
+            // 
+            // rsizeTB
+            // 
+            this.rsizeTB.Location = new System.Drawing.Point(6, 38);
+            this.rsizeTB.Name = "rsizeTB";
+            this.rsizeTB.Size = new System.Drawing.Size(131, 20);
+            this.rsizeTB.TabIndex = 4;
+            this.rsizeTB.Text = "144;26";
+            // 
+            // rposTB
+            // 
+            this.rposTB.Location = new System.Drawing.Point(152, 38);
+            this.rposTB.Name = "rposTB";
+            this.rposTB.Size = new System.Drawing.Size(128, 20);
+            this.rposTB.TabIndex = 5;
+            this.rposTB.Text = "390;90";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Rect size";
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(149, 22);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(50, 13);
+            this.label.TabIndex = 7;
+            this.label.Text = "Rect pos";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 64);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(274, 37);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Apply";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(113, 114);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(167, 21);
+            this.comboBox1.TabIndex = 9;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(9, 116);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(98, 17);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "Check similarity";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(6, 162);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(274, 119);
+            this.panel1.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 284);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "SIMILARITY";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(6, 298);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(274, 23);
+            this.progressBar1.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(295, 376);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.rposTB);
+            this.Controls.Add(this.rsizeTB);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox rsizeTB;
+        private System.Windows.Forms.TextBox rposTB;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ LoL_MatchAccepter/Form1.cs
@@ -0,0 +1,181 @@
+﻿using LoL_MatchAccepter.Properties;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Globalization;
+using System.Linq;
+using System.Resources;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoL_MatchAccepter
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr FindWindow(string strClassName, string strWindowName);
+
+        [DllImport("user32.dll")]
+        public static extern bool GetWindowRect(IntPtr hwnd, ref Rect rectangle);
+        
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint dwData, int dwExtraInfo);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
+        
+        public const int BM_CLICK = 0x00F5;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SendMessage(IntPtr hwnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+        private IntPtr ptr;
+
+        private Point rsize, rpos;
+
+        public struct Rect
+        {
+            public int Left { get; set; }
+            public int Top { get; set; }
+            public int Right { get; set; }
+            public int Bottom { get; set; }
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            Image imgToSave = pictureBox1.Image;
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+                imgToSave.Save(sfd.FileName + ".jpg");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+            ResourceManager MyResourceClass = new ResourceManager(typeof(Resources));
+            ResourceSet resourceSet = MyResourceClass.GetResourceSet(CultureInfo.CurrentUICulture, true, true);
+            foreach (DictionaryEntry entry in resourceSet)
+            {
+                comboBox1.Items.Add(entry.Key.ToString());
+            }
+
+            comboBox1.SelectedIndex = 0;
+
+            SetRects();
+        }
+
+        private void SetRects()
+        {
+            string[] s = rsizeTB.Text.Split(';');
+            string[] s1 = rposTB.Text.Split(';');
+             
+            rsize = new Point(int.Parse(s[0]), int.Parse(s[1]));
+            rpos = new Point(int.Parse(s1[0]), int.Parse(s1[1]));
+        }
+
+        private int ImageSimilarity(Bitmap img1, Bitmap img2)
+        {
+            int count1 = 0, count2 = 0;
+            bool flag = true;
+            string img1_ref, img2_ref;
+
+            if (img1.Width == img2.Width && img1.Height == img2.Height)
+            {
+                for (int i = 0; i < img1.Width; i++)
+                {
+                    for (int j = 0; j < img1.Height; j++)
+                    {
+                        img1_ref = img1.GetPixel(i, j).ToString();
+                        img2_ref = img2.GetPixel(i, j).ToString();
+                        if (img1_ref != img2_ref)
+                        {
+                            count2++;
+                            flag = false;
+                            break;
+                        }
+                        count1++;
+                    }
+                }
+
+                return count1;
+            }
+            else
+                return 0;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SetRects();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Bitmap bm = (Bitmap)Resources.ResourceManager.GetObject(comboBox1.Text);
+            progressBar1.Maximum = bm.Width * bm.Height;
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            Process[] processes = Process.GetProcessesByName("LeagueClientUx");
+            Process lol = processes[0];
+            ptr = lol.MainWindowHandle;
+
+            Rect rect = new Rect();
+            GetWindowRect(ptr, ref rect);
+
+            Rectangle _rect = new Rectangle(0, 0, rsize.X, rsize.Y);
+
+            Bitmap bmp = new Bitmap(_rect.Width, _rect.Height, PixelFormat.Format32bppArgb);
+            Graphics g = Graphics.FromImage(bmp);
+
+            g.CopyFromScreen(rect.Left + rpos.X, rect.Top + rpos.Y, 0, 0, bmp.Size, CopyPixelOperation.SourceCopy);
+
+            pictureBox1.Image = bmp;
+
+            if (checkBox1.Checked)
+            {
+
+                Bitmap bm = (Bitmap)Resources.ResourceManager.GetObject(comboBox1.Text);
+                int sim = ImageSimilarity(bm, bmp);
+
+                progressBar1.Value = sim;
+
+                if (sim > (bm.Width*bm.Height)*0.8)
+                {
+                    panel1.BackColor = Color.Green;
+                    //Cursor.Position = new Point(rect.Left + 640, rect.Top + 560);
+                    //mouse_event(Mouse.MOUSEEVENTF_LEFTDOWN | Mouse.MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                }
+                else
+                    panel1.BackColor = Color.Red;
+            }
+            else
+                panel1.BackColor = Color.Blue;
+
+        }
+
+    }
+
+    internal class Mouse
+    {
+        public const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        public const int MOUSEEVENTF_LEFTUP = 0x04;
+        public const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        public const int MOUSEEVENTF_RIGHTUP = 0x10;
+    }
+}
